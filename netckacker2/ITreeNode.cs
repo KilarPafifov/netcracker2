562375bf591f539de2d2e4e4e0235104ae986ed7
Kilar Pafifov <apafifova@list.ru>
2018-05-15 14:53:03 +0300

    add class TreeNode with interface

---
+++ netckacker2/ITreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netckacker2
+{
+    public interface ITreeNode
+    {
+        ITreeNode GetParent();
+        void SetParent(ITreeNode parent);
+        ITreeNode GetRoot();
+        bool IsLeaf();
+        int GetChildCount();
+        void AddChild(ITreeNode child);
+        bool RemoveChild(ITreeNode child);
+    }
+}
+
---
+++ netckacker2/TreeNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netckacker2
+{
+    class TreeNode : ITreeNode
+    {
+        private int data;
+        private ITreeNode parent;
+        private HashSet<ITreeNode> childset;
+        public ITreeNode GetParent()
+        {
+            return parent;
+        }
+
+        public ITreeNode GetRoot()
+        {
+            if (this.parent == null)
+            {
+                return null;
+            }
+            return null; //доделать самому
+        }
+
+        public void SetParent(ITreeNode parent)
+        {
+            this.parent = parent;
+        }
+
+        public bool IsLeaf()
+        {
+            if (childset.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int GetChildCount()
+        {
+            return childset.Count;
+        }
+
+        public void AddChild(ITreeNode child)
+        {
+            childset.Add(child);
+            child.SetParent(this);
+        }
+
+        public bool RemoveChild(ITreeNode child)
+        {
+            return true;
+        }
+    }
+}
