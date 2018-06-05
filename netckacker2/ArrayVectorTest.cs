a0410797d973d4d976f4d55d69542155c7ae812e
Kilar Pafifov <apafifova@list.ru>
2018-06-05 18:16:32 +0300

    add methods GetMax, GetMin and Mult with tests (ArrayVector)

--- netckacker2/ArrayVectorTest.cs
+++ netckacker2/ArrayVectorTest.cs
@@ -57,9 +57,119 @@ namespace netckacker2
             return true;
         }
         
+        public static bool TestGetMax()
+        {
+            IArrayVector expected = new ArrayVector();
+            double[] mas = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                mas[i] = i;
+            }
+
+            expected.Set(mas);
+
+            mas[0] += 10;
+            if(expected.GetMax() != mas[0])
+            {
+                return false;
+            }
+
+            mas[1] += 10;
+            if (expected.GetMax() != mas[1])
+            {
+                return false;
+            }
+
+            mas[2] += 10;
+            if (expected.GetMax() != mas[2])
+            {
+                return false;
+            }
+
+            mas[3] += 10;
+            if (expected.GetMax() != mas[3])
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TestGetMin()
+        {
+            IArrayVector expected = new ArrayVector();
+            double[] mas = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                mas[i] = i;
+            }
+
+            expected.Set(mas);
+
+            mas[3] -= 10;
+            if (expected.GetMin() != mas[3])
+            {
+                return false;
+            }
+
+            mas[2] -= 10;
+            if (expected.GetMin() != mas[2])
+            {
+                return false;
+            }
+
+            mas[1] -= 10;
+            if (expected.GetMin() != mas[1])
+            {
+                return false;
+            }
+
+            mas[0] -= 10;
+            if (expected.GetMin() != mas[0])
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TestMult()
+        {
+            IArrayVector array = new ArrayVector();
+            double[] mas = new double[4];
+            int factor = 2;
+
+            for (int i = 0; i < 4; i++)
+            {
+                mas[i] = i;
+            }
+
+            array.Set(mas);
+            array.Mult(factor);
+            double[] expected = array.Get();
+
+            double[] actual = new double[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                actual[i] = i * factor;
+            }
+
+            for(int i = 0; i < 4; i++)
+            {
+                if(actual[i] != expected[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
         private static void Main(string[] args)
         {
-            Console.WriteLine(TestSetByIndex());
+            Console.WriteLine(TestMult());
             Console.ReadLine();
         }
     }
